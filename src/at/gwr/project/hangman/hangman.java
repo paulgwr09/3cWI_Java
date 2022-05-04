e2ae491f096194c0cb561ed177aac2bf633f7c1a
paulgwr09 <paul.gwiggner@student.htldornbirn.at>
2022-05-04 08:32:49 +0200

    hangman angefangen

--- src/at/gwr/project/hangman/hangman.java
+++ src/at/gwr/project/hangman/hangman.java
@@ -1,7 +1,27 @@
 package at.gwr.project.hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        start(scanner);
+    }
+
+    public static void start(Scanner scanner) {
+
+
+        String[] arr;
+        arr = new String[1];
+
+        arr[0] = scanner.next();
+        int stringlength = arr[0].length();
+
+        for (int i = 0; i == arr[0].length(); i++) {
+            System.out.print("-")
+        }
+
+
+
     }
 }
